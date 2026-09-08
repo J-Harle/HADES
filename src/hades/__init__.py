e48c0e8f07adbce7358e0eb63f21072b592a8a7b
Josh Harle <158578157+J-Harle@users.noreply.github.com>
2026-09-08 13:23:57 +0100

    Add files via upload

--- src/hades/__init__.py
+++ src/hades/__init__.py
@@ -1,11 +1,54 @@
-from .evaluator import EnergeticMaterialEvaluator
-from .eof import IsodesmicEnthalpyCalculator
-from .oxygen_balance import oxygen_balance_from_smiles
-from .detonation import calculate_detonation_properties
+"""
+HADES
+=====
+
+High-throughput Analysis for the Design of Energetic Systems.
+"""
+
+# Provide convenient module aliases
+from . import isodesmic as eof
+from . import det_v_p as detonation
+
+# Functions that currently exist
+from .oxygen_balance import (
+    atom_counts_from_smiles,
+    oxygen_balance,
+)
+
+from .isodesmic import (
+    calculate_hf,
+    calculate_hr,
+    filter_and_average,
+)
+
+from .det_v_p import (
+    calc_gas_products,
+    calc_phi,
+    det_p,
+    det_v,
+)
+
+
+__version__ = "1.0.0"
+
 
 __all__ = [
-    "EnergeticMaterialEvaluator",
-    "IsodesmicEnthalpyCalculator",
-    "oxygen_balance_from_smiles",
-    "calculate_detonation_properties",
+    # Module aliases
+    "eof",
+    "detonation",
+
+    # Oxygen balance
+    "atom_counts_from_smiles",
+    "oxygen_balance",
+
+    # Enthalpy of formation
+    "calculate_hf",
+    "calculate_hr",
+    "filter_and_average",
+
+    # Detonation properties
+    "calc_gas_products",
+    "calc_phi",
+    "det_v",
+    "det_p",
 ]
